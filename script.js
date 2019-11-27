3cbfd013cda81fbad7f7ec605a2497829452bf7c
Cleanly <oskar.turesson99@gmail.com>
2019-11-27 11:12:39 +0100

    added put all the play functions in an function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const shuffle = function(array) {
 
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    currentIndex--;
 
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
@@ -31,47 +31,56 @@ const makeArray = function() {
 }
 
 // console.log(memoryPieces);
+window.document.getElementsByClassName('buttons')['start'].addEventListener('click', function(){
+  var index = 0;
+    const memoryPieces = makeArray();
+    memoryPieces.forEach(function(memoryPiece){
+    const divs = document.createElement("div");
+    const title =  document.createElement("p");
+    title.innerHTML = memoryPiece.number;
+    divs.setAttribute('class', 'memoryPiece');
+    title.setAttribute('class', 'hej');
+    divs.setAttribute('data-number', memoryPiece.number);
+    document.body.getElementsByClassName('memoryContainer')[0].appendChild(divs);
+    document.body.getElementsByClassName('memoryPiece')[index].appendChild(title);
+    index++;
+  })
+  
+  const numberOfMemoryPieces = window.document.getElementsByClassName('memoryPiece');
 
-var index = 0;
-  const memoryPieces = makeArray();
-  memoryPieces.forEach(function(memoryPiece){
-  const divs = document.createElement("div");
-  // const image = document.createElement("img");
-  const title =  document.createElement("p");
-  title.innerHTML = memoryPiece.number;
-  // image.setAttribute('src', ingredient.image);
-  divs.setAttribute('class', 'memoryPiece');
-  title.setAttribute('class', 'hej');
-  divs.setAttribute('data-number', memoryPiece.number);
-  document.body.getElementsByClassName('memoryContainer')[0].appendChild(divs);
-  // document.body.getElementsByClassName('fruitDivs')[index].appendChild(image);
-  document.body.getElementsByClassName('memoryPiece')[index].appendChild(title);
-  index++;
+  for (var i = 0; i < numberOfMemoryPieces.length; i++) {
+    numberOfMemoryPieces[i].addEventListener('click', function(){
+      
+      let theEventTarget = event.target;
+      
+      if (theEventTarget.className != 'memoryPiece') {
+        
+        theEventTarget = event.path[1];
+        
+      }
+      if (theEventTarget.className === 'memoryPiece') {
+        // console.log(theEventTarget.getElementsByClassName('hej')[0].classList.add('hej1'))
+        theEventTarget.classList.add('hej1');
+      }
+      let prevCard = "";
+      const currentCard = theEventTarget.dataset.number;
+      if (prevCard === "") {
+        prevCard = currentCard
+      }
+      if (prevCard !== "") {
+        
+      }
+      
+      console.log(currentCard)
+      console.log(prevCard)
+      // console.log(event)
+    })
+  }
 })
 
 
-const numberOfMemoryPieces = window.document.getElementsByClassName('memoryPiece');
 
-for (var i = 0; i < numberOfMemoryPieces.length; i++) {
-  numberOfMemoryPieces[i].addEventListener('click', function(){
-    
-    let theEventTarget = event.target;
-    
-    if (theEventTarget.className != 'memoryPiece') {
-      
-      theEventTarget = event.path[1];
-      
-    }
-    if (theEventTarget.className === 'memoryPiece') {
-      // console.log(theEventTarget.getElementsByClassName('hej')[0].classList.add('hej1'))
-      theEventTarget.classList.add('hej1');
-    }
-    if (theEventTarget.textContent === '1') {
-      console.log('hej')
-    }
-    // console.log(event)
-  })
-}
+
 
 
 
